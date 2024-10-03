67b0844ccdcfceffe6de54b70c69a738478f6667
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-10-03 12:28:34 +0300

    Add files via upload

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 var WebApp = window.Telegram.WebApp;
     
 WebApp.expand()
+WebApp.headerColor = '#1e1e24'
+WebApp.backgroundColor = '#1e1e24';
+WebApp.bottomBarColor = '#1e1e24'
 
 setTimeout(()=>{
     WebApp.HapticFeedback.impactOccurred('light')
