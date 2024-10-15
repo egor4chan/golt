a806dedb1955d1a4eb3957641fb4ad8dc14345d6
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-10-15 22:55:11 +0400

    Add files via upload

--- js/Level.js
+++ js/Level.js
@@ -1,15 +1,23 @@
 const windowWidth = Number(window.innerWidth);
+let noty = document.getElementById('scale');
+
 
 function levelUp() {
     let levelBoard = document.getElementById('levelBoard');
     let progress = window.getComputedStyle(levelBoard).width;
     
-    levelBoard.style.width = `calc(${progress} + 1%)`
+    levelBoard.style.width = `calc(${progress} + 4%)`
     progress = Number(progress.slice(0, -2));
     
     // если ШКАЛА уровня заполнена, выполнить...
     if (progress >= windowWidth) {
         levelBoard.style.width = '1%';
+
+        // уведомление о заполнении шкалы
+        noty.style.top = '10px';
+        setTimeout(()=>{ noty.style.top = '-100px'}, 3000)
+
+        // начисление денег
     }
     // если ШКАЛА еще не заполнена
     else {
