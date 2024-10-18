940a3834e70b9bf07b722cb18e4f9df180891d30
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-10-18 17:29:46 +0400

    Add files via upload

--- js/Level.js
+++ js/Level.js
@@ -2,7 +2,7 @@ const windowWidth = Number(window.innerWidth);
 let noty = document.getElementById('scale');
 let levelBoard = document.getElementById('levelBoard');
 
-const levelScalePercents = {1: '4%', 2: '2%', 3: '1.2%', 4: '1.1%', 5: '1%', 6: '0.9%', 7: '0.8%', 8: '0.7%', 9: '0.6%', 10: '0.5%', 11: '0.4%', 12: '0.3', 13: '0.25%', 14: '0.2%', 15: '0.15%', 16: '0.125%', 17: '0.1%', 18: '0.075%'}
+const levelScalePercents = {1: '4%', 2: '2%', 3: '1.2%', 4: '1.1%', 5: '1%', 6: '0.9%', 7: '0.8%', 8: '0.7%', 9: '0.6%', 10: '0.5%', 11: '0.4%', 12: '0.3', 13: '0.25%', 14: '0.2%', 15: '0.15%', 16: '0.125%', 17: '0.1%', 18: '0.075%', 19: '0.075%', 20: '0.075%', 21: '0.075%', 22: '0.075%', 23: '0.075%', 24: '0.075%', 25: '0.075%', 26: '0.075%', 27: '0.075%'}
 
 levelBoard.style.width = `${window.localStorage.getItem('levelWidth')}`
 
